fc9ad4fb5e13abdb99c01759f27cfb6b88ab711c
CKodes <ckodes@outlook.com>
2024-02-29 16:44:28 +0800

    feat: add getBlocks to retrieve childBlocks

--- lib/notion.tsx
+++ lib/notion.tsx
@@ -37,3 +37,55 @@ export const getPageFromSlug = async (slug: any) => {
   }
   return {}
 }
+
+export const getBlocks = async (blockID: any): Promise<any[]> => {
+  const blockId = blockID.replaceAll('-', '')
+
+  const { results } = await notion.blocks.children.list({
+    block_id: blockId,
+    page_size: 100,
+  })
+
+  const childBlocks = results.map(async (block: any) => {
+    if (block.has_children) {
+      const children = await getBlocks(block.id)
+      return { ...block, children }
+    }
+    return block
+  })
+
+  return Promise.all(childBlocks).then((blocks) =>
+    blocks.reduce((acc, curr) => {
+      if (curr.type === 'bulleted_list_item') {
+        if (acc[acc.length - 1]?.type === 'bulleted_list') {
+          acc[acc.length - 1][acc[acc.length - 1].type].children?.push(curr)
+        } else {
+          acc.push({
+            id: getRandomInt(10 ** 99, 10 ** 100).toString(),
+            type: 'bulleted_list',
+            bulleted_list: { children: [curr] },
+          })
+        }
+      } else if (curr.type === 'numbered_list_item') {
+        if (acc[acc.length - 1]?.type === 'numbered_list') {
+          acc[acc.length - 1][acc[acc.length - 1].type].children?.push(curr)
+        } else {
+          acc.push({
+            id: getRandomInt(10 ** 99, 10 ** 100).toString(),
+            type: 'numbered_list',
+            numbered_list: { children: [curr] },
+          })
+        }
+      } else {
+        acc.push(curr)
+      }
+      return acc
+    }, [])
+  )
+}
+
+function getRandomInt(minimum: number, maximum: number) {
+  const min = Math.ceil(minimum)
+  const max = Math.floor(maximum)
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
