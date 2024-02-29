6014565b40e4d54ac13553e4e65fed48b7cb8a51
CKodes <ckodes@outlook.com>
2024-02-29 12:32:15 +0800

    feat: add getPageFromSlug
    This commit will add a function to get the Slug property from the database query which will be used for routing to individual pages

--- lib/notion.tsx
+++ lib/notion.tsx
@@ -17,3 +17,23 @@ export const getDatabase = async () => {
   saveResultsJson('getDatabase.json', response.results)
   return response.results
 }
+
+export const getPageFromSlug = async (slug: any) => {
+  checkTestCodesFolder()
+  const response = await notion.databases.query({
+    database_id: databaseId as any,
+    filter: {
+      property: 'Slug',
+      formula: {
+        string: {
+          equals: slug,
+        },
+      },
+    },
+  })
+  if (response?.results?.length) {
+    saveResultsJson('getPageFromSlug.json', response)
+    return response?.results?.[0]
+  }
+  return {}
+}
