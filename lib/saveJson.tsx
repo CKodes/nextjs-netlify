2baa9fcdce52c4de02e075068c17340ac52fe9e6
CKodes <ckodes@outlook.com>
2024-02-29 10:38:55 +0800

    chore: [TESTCODES]
    This commit will add testcodes that will save responses gotten from Notion API in json format

---
+++ lib/saveJson.tsx
@@ -0,0 +1,19 @@
+/* ----- Testcodes to save reponses of queries in JSON format --- */
+// TODO: REMOVE WHEN RAISING PR
+
+import fs from 'fs'
+import path from 'path'
+
+const projectRoot = process.cwd()
+const testCodesFolderPath = path.join(projectRoot, 'test_codes')
+
+export const checkTestCodesFolder = () => {
+  if (!fs.existsSync(testCodesFolderPath)) {
+    fs.mkdirSync(testCodesFolderPath)
+  }
+}
+
+export const saveResultsJson = (fileName: string, response: any) => {
+  const filePath = path.join(testCodesFolderPath, fileName)
+  fs.writeFileSync(filePath, JSON.stringify({ response }, null, 2))
+}
