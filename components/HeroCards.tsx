f7b476c7634e08ea52e0a9d9f877a25a95b5f678
CKodes <ckodes@outlook.com>
2024-03-04 01:08:24 +0800

    feat: add HeroCards component

---
+++ components/HeroCards.tsx
@@ -0,0 +1,34 @@
+'use client'
+import '@govtechsg/sgds/css/sgds.css'
+import HighlightsCard from './HighlightsCards'
+import styles from './hero.module.css'
+
+export default function HeroCardsContainer() {
+  return (
+    <>
+      {/* Hero Cards Section */}
+      <section className={styles.offset}>
+        <div className={styles.heroCardsContainer}>
+          <div className={styles.heroCards}>
+            <HighlightsCard
+              cardTitle="Title 1"
+              cardDescription="oh captain my captain"
+            />
+            <HighlightsCard
+              cardTitle="Title 2"
+              cardDescription="oh captain my captain"
+            />
+            <HighlightsCard
+              cardTitle="Title 3"
+              cardDescription="oh captain my captain"
+            />
+            <HighlightsCard
+              cardTitle="Title 4"
+              cardDescription="oh captain my captain"
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
