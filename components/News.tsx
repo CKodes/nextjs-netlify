e91ad5f2432e7263dc12f3b7bee5e82ad3f6e032
CKodes <ckodes@outlook.com>
2024-03-08 13:27:08 +0800

    feat: add NewsContainerProps and utilize SideNewsCard
    This commit will:
    
    - Replace hardcoded Card component with SideNewsCard and set max instance to two
    
    - Add NewsContainerProps

--- components/News.tsx
+++ components/News.tsx
@@ -3,8 +3,23 @@ import '@govtechsg/sgds/css/sgds.css'
 import { Button } from '@govtechsg/sgds-react/Button'
 import { Card } from '@govtechsg/sgds-react/Card'
 import styles from './landingPage.module.css'
+import SideNewsCard from './SideNewsCard'
 
-export default function NewsContainer() {
+interface NewsContainerProps {
+  mainNewsTitle: string
+  mainNewsLink: string
+  mainNewsSnippet: string
+  sideNewsCardTitleArray: string[]
+  sideNewsCardSnippetArray: string[]
+}
+
+export default function NewsContainer({
+  mainNewsTitle,
+  mainNewsLink,
+  mainNewsSnippet,
+  sideNewsCardTitleArray,
+  sideNewsCardSnippetArray,
+}: NewsContainerProps) {
   return (
     <>
       {/* News Section */}
@@ -17,44 +32,30 @@ export default function NewsContainer() {
           <Card className="border-0">
             <Card.Img
               alt="The team achieves a milestone"
-              src="https://images.unsplash.com/photo-1591115765373-5207764f72e7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cHJlc2VudGF0aW9ufGVufDB8fDB8fHww"
+              src={mainNewsLink}
               variant="top"
             />
             <Card.Body>
               <span>Tags</span>
-              <h3>Card Title</h3>
-              <Card.Text>
-                {`Some quick example text to build on the card title and make up
-                the bulk of the card's content.`}
-              </Card.Text>
+              <h3>{mainNewsTitle}</h3>
+              <Card.Text>{mainNewsSnippet}</Card.Text>
               <a href="www.google.com">Read More</a>
             </Card.Body>
           </Card>
           <div className={styles.newsArchiveContainer}>
             <hr className="d-block d-md-none" />
-            <div className={styles.newsArchiveSnippet}>
-              <span>Tags</span>
-              <h3>Some Title</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Perferendis quod ipsa consequatur ratione nisi fuga quibusdam
-                facere ullam dolores asperiores et voluptatibus dolore hic
-                nesciunt sed, necessitatibus temporibus sapiente dolorum?
-              </p>
-              <a href="www.google.com">Read More</a>
-            </div>
-            <hr />
-            <div className={styles.newsArchiveSnippet}>
-              <span>Tags</span>
-              <h3>Some Title</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Perferendis quod ipsa consequatur ratione nisi fuga quibusdam
-                facere ullam dolores asperiores et voluptatibus dolore hic
-                nesciunt sed, necessitatibus temporibus sapiente dolorum?
-              </p>
-              <a href="www.google.com">Read More</a>
-            </div>
+            {sideNewsCardTitleArray.slice(0, 2).map((title, index) => (
+              <SideNewsCard
+                key={index}
+                sideNewsCardTitle={title}
+                sideNewsCardSnippet={
+                  sideNewsCardSnippetArray &&
+                  index < sideNewsCardSnippetArray.length
+                    ? sideNewsCardSnippetArray[index]
+                    : ''
+                }
+              />
+            ))}
           </div>
           <Button className="d-block d-md-none w-100">All Articles</Button>
         </div>
