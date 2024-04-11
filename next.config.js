7907310778e8e751c357e0ab89a048e2fdfd762d
Zui Young <younglim@users.noreply.github.com>
2024-04-11 16:44:55 +0800

    Use process.env in next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
 }
 
 module.exports = {
+  env: {
+    NOTION_TOKEN: process.env.NOTION_TOKEN || require('dotenv').config().parsed?.NOTION_TOKEN,
+    NOTION_DATABASE_ID: process.env.NOTION_DATABASE_ID || require('dotenv').config().parsed?.NOTION_DATABASE_ID,
+  },
   nextConfig,
   images: {
     remotePatterns: [
