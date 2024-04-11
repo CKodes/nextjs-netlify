67888d11775615d95c7d891276b5566cba8a9e1e
CKodes <ckodes@outlook.com>
2024-04-11 11:20:39 +0800

    feat[BREAKING CHANGE]: config for github actions and deployment to github pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: '/nextjs-netlify',
+  output: 'export', // <=== enables static exports
+}
 
 module.exports = {
+  nextConfig,
   images: {
     remotePatterns: [
       {
