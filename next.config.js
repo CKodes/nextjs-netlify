31ebde03366da5634ccd696948b3cc7774c58b61
CKodes <ckodes@outlook.com>
2024-04-16 13:27:19 +0800

    chore: add / to basePath

--- next.config.js
+++ next.config.js
@@ -2,11 +2,15 @@
 
 module.exports = {
   env: {
-    NOTION_TOKEN: process.env.NOTION_TOKEN || require('dotenv').config().parsed?.NOTION_TOKEN,
-    NOTION_DATABASE_ID: process.env.NOTION_DATABASE_ID || require('dotenv').config().parsed?.NOTION_DATABASE_ID,
+    NOTION_TOKEN:
+      process.env.NOTION_TOKEN ||
+      require('dotenv').config().parsed?.NOTION_TOKEN,
+    NOTION_DATABASE_ID:
+      process.env.NOTION_DATABASE_ID ||
+      require('dotenv').config().parsed?.NOTION_DATABASE_ID,
   },
   distDir: 'out',
   output: 'export',
-  assetPrefix: "/",
-  basePath: "nextjs-netlify",
+  assetPrefix: '/',
+  basePath: '/nextjs-netlify',
 }
