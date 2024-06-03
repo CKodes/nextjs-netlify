2755b85b2a967e964bd00ca24a44186480cda04c
CKodes <ckodes@outlook.com>
2024-06-03 14:52:28 +0800

    chore: testcodes

--- next.config.js
+++ next.config.js
@@ -1,51 +1,51 @@
 /** @type {import('next').NextConfig} */
 
-module.exports = {
-  env: {
-    NOTION_TOKEN:
-      process.env.NOTION_TOKEN ||
-      require('dotenv').config().parsed?.NOTION_TOKEN,
-    NOTION_DATABASE_ID:
-      process.env.NOTION_DATABASE_ID ||
-      require('dotenv').config().parsed?.NOTION_DATABASE_ID,
-  },
-  distDir: 'out',
-  output: 'export',
-  basePath: '/nextjs-netlify',
-}
-
-// let config = {}
-
-// console.log('process.argv:', process.argv)
-
-// if (process.argv.includes('build')) {
-//   // Configuration for npm run build
-//   config = {
-//     env: {
-//       NOTION_TOKEN:
-//         process.env.NOTION_TOKEN ||
-//         require('dotenv').config().parsed?.NOTION_TOKEN,
-//       NOTION_DATABASE_ID:
-//         process.env.NOTION_DATABASE_ID ||
-//         require('dotenv').config().parsed?.NOTION_DATABASE_ID,
-//     },
-//     distDir: 'out',
-//     output: 'export',
-//     basePath: '/nextjs-netlify',
-//   }
-// } else {
-//   // Configuration for npm run dev
-//   config = {
-//     images: {
-//       remotePatterns: [
-//         {
-//           hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
-//         },
-//       ],
-//     },
-//   }
+// module.exports = {
+//   env: {
+//     NOTION_TOKEN:
+//       process.env.NOTION_TOKEN ||
+//       require('dotenv').config().parsed?.NOTION_TOKEN,
+//     NOTION_DATABASE_ID:
+//       process.env.NOTION_DATABASE_ID ||
+//       require('dotenv').config().parsed?.NOTION_DATABASE_ID,
+//   },
+//   distDir: 'out',
+//   output: 'export',
+//   basePath: '/nextjs-netlify',
 // }
 
-// console.log('config is' + JSON.stringify(config, null, 2))
+let config = {}
 
-// module.exports = config
+console.log('process.argv:', process.argv)
+
+if (process.argv.includes('build')) {
+  // Configuration for npm run build
+  config = {
+    env: {
+      NOTION_TOKEN:
+        process.env.NOTION_TOKEN ||
+        require('dotenv').config().parsed?.NOTION_TOKEN,
+      NOTION_DATABASE_ID:
+        process.env.NOTION_DATABASE_ID ||
+        require('dotenv').config().parsed?.NOTION_DATABASE_ID,
+    },
+    distDir: 'out',
+    output: 'export',
+    basePath: '/nextjs-netlify',
+  }
+} else {
+  // Configuration for npm run dev
+  config = {
+    images: {
+      remotePatterns: [
+        {
+          hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
+        },
+      ],
+    },
+  }
+}
+
+console.log('config is' + JSON.stringify(config, null, 2))
+
+module.exports = config
