97f15479559d2049d73678f2811acf54d7c28b2b
CKodes <ckodes@outlook.com>
2024-06-04 10:10:45 +0800

    fix: update config options depending on production or development environment

--- next.config.js
+++ next.config.js
@@ -1,15 +1,27 @@
 /** @type {import('next').NextConfig} */
 
-module.exports = {
-  env: {
+const nextConfig = {}
+
+if (process.env.NODE_ENV === 'production') {
+  nextConfig.env = {
     NOTION_TOKEN:
       process.env.NOTION_TOKEN ||
       require('dotenv').config().parsed?.NOTION_TOKEN,
     NOTION_DATABASE_ID:
       process.env.NOTION_DATABASE_ID ||
       require('dotenv').config().parsed?.NOTION_DATABASE_ID,
-  },
-  distDir: 'out',
-  output: 'export',
-  basePath: '/nextjs-netlify',
+  }
+  nextConfig.distDir = 'out'
+  nextConfig.output = 'export'
+  nextConfig.basePath = '/nextjs-netlify'
+} else if (process.env.NODE_ENV === 'development') {
+  nextConfig.images = {
+    remotePatterns: [
+      {
+        hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
+      },
+    ],
+  }
 }
+
+module.exports = nextConfig
